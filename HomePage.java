db089dc28e58fdc303b5f9cf3b59b7bdd1888f5f
Ghazala Babar <ghazala.babar@emumba.com>
2024-07-12 03:04:07 +0500

    HomePage.java

--- HomePage.java
+++ HomePage.java
@@ -1,5 +1,23 @@
-public DropdownPage clickDropDown(){
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class Homepage {
+    private WebDriver driver;
+    private By formAuthenticationLink = By.linkText("Form Authentication");
+    public Homepage(WebDriver driver){
+        this.driver = driver;
+    }
+    public DropdownPage clickDropDown(){
     clickLink("Dropdown");
     return new DropdownPage(driver);
 }
+    public LoginPage clickFormAuthentication(){
+        driver.findElement(formAuthenticationLink).click();
+       return new LoginPage(driver);
+    }
+    
+}
+
 
