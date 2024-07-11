4cb79f5c38ba0e301f421b6ffc2cd0451c0e769b
Ghazala Babar <ghazala.babar@emumba.com>
2024-07-12 02:47:57 +0500

    Create LoginTests.java

---
+++ LoginTests.java
@@ -0,0 +1,19 @@
+package login;
+
+import base.BaseTests;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.LoginPage;
+import pages.SecureAreaPage;
+
+public class LoginTests extends BaseTests {
+
+    @Test
+    public void testSuccessfulLogin(){
+        LoginPage loginPage = homePage.clickFormAuthentication();
+        loginPage.setUsername("tomsmith");
+        loginPage.setPassword("SuperSecretPassword!");
+        SecureAreaPage secureAreaPage = loginPage.clickLoginButton();
+        assertEquals(secureAreaPage.getAlertText(), "You logged into a secure area! x", "Alert text is incorrect");
+    }
+}
