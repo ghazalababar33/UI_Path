27930b85836d2d9c85f39bedf74f151b9efb1064
Ghazala Babar <ghazala.babar@emumba.com>
2024-07-12 03:33:29 +0500

    JavaScriptTests.java

--- JavaScriptTests.java
+++ JavaScriptTests.java
@@ -9,4 +9,8 @@ public class JavaScriptTests extends BaseTests {
     public void testScrollToTable(){
         homePage.clickLargeAndDeepDom().scrollToTable();
     }
+    @Test
+public void testScrollToFifthParagraph(){
+    homePage.clickInfiniteScroll().scrollToParagraph(5);
+}
 }
